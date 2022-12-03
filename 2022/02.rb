c29810dce2b5d893be58a6d161d531f80d48d252
Sunny Ripert <sunny@sunfox.org>
2022-12-03 10:54:37 +0100

    02 fixes

--- 2022/02.rb
+++ 2022/02.rb
@@ -2,22 +2,20 @@ input = "A Y
 B X
 C Z"
 
-input = File.read("02.txt")
+# input = File.read("02.txt")
 
-outcome_score = {
-  ["A", "X"] => 0 + 3, # lose rock/scissors
-  ["A", "Y"] => 3 + 1, # draw rock/rock
-  ["A", "Z"] => 6 + 2, # win rock/paper
-  ["B", "X"] => 0 + 1, # lose paper/rock
-  ["B", "Y"] => 3 + 2, # draw paper/paper
-  ["B", "Z"] => 6 + 3, # win paper/scissors
-  ["C", "X"] => 0 + 2, # lose scissors/paper
-  ["C", "Y"] => 3 + 3, # draw scissors/scissors
-  ["C", "Z"] => 6 + 1, # win scissors/rock
+scores = {
+  "A X" => 0 + 3, # lose rock/scissors
+  "A Y" => 3 + 1, # draw rock/rock
+  "A Z" => 6 + 2, # win rock/paper
+
+  "B X" => 0 + 1, # lose paper/rock
+  "B Y" => 3 + 2, # draw paper/paper
+  "B Z" => 6 + 3, # win paper/scissors
+
+  "C X" => 0 + 2, # lose scissors/paper
+  "C Y" => 3 + 3, # draw scissors/scissors
+  "C Z" => 6 + 1, # win scissors/rock
 }
 
-sum = input.lines.map { _1.split }.sum do |a, b|
-  outcome_score[[a, b]]
-end
-
-p sum
+p input.split("\n").sum { scores[_1] }
