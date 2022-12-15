a2268d700038d34566e0adc91fb3031d99e43b8f
Sunny Ripert <sunny@sunfox.org>
2022-12-15 12:29:53 +0100

    15

---
+++ 2022/15/15.rb
@@ -0,0 +1,187 @@
+input = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+"
+
+class Scan
+  attr_reader :grid
+
+  def initialize(min_x: nil, max_x: nil, min_y: nil, max_y: nil)
+    @grid = Hash.new { |h, x| h[x] = Hash.new { |l, y| l[y] = nil } }
+    @min_x = min_x
+    @max_x = max_x
+    @min_y = min_y
+    @max_y = max_y
+  end
+
+  def read(input)
+    input.each_line do |line|
+      coordinates = line.scan(/([-\d]+)/).flatten.map(&:to_i)
+      sensor_x, sensor_y, beacon_x, beacon_y = coordinates
+      beacon = Beacon.new(beacon_x, beacon_y)
+      sensor = Sensor.new(sensor_x, sensor_y, beacon)
+      self[sensor_x, sensor_y] = sensor
+      self[beacon_x, beacon_y] = beacon
+    end
+  end
+
+  def []=(x, y, value)
+    grid[x][y] = value unless grid[x][y]
+  end
+
+  def [](x, y) = grid[x][y]
+
+  def min_x = @min_x ||= grid.keys.min
+  def max_x = @max_x ||= grid.keys.max
+  def min_y = @min_y ||= grid.values.map(&:keys).flatten.min
+  def max_y = @max_y ||= grid.values.map(&:keys).flatten.max
+
+  def draw(from_x = min_x, to_x = max_x, from_y = min_y, to_y = max_y)
+    from_y.upto(to_y) do |y|
+      from_x.upto(to_x) do |x|
+        v = grid[x][y]
+        case v
+        when 0 then print "#"
+        when nil then print "."
+        else print v.to_s
+        end
+      end
+      puts
+    end
+  end
+
+  def beacon?(x, y) = grid[x][y].is_a?(Beacon)
+  def sensor?(x, y) = grid[x][y].is_a?(Sensor)
+  def sensors = grid.values.flat_map(&:values).select { |v| v.is_a?(Sensor) }
+
+  def fill
+    sensors.each do |s|
+      fill_zone(s)
+    end
+  end
+
+  def fill_zone(s, target_y = nil)
+    h = 0
+    p [s.min_x..s.max_x]
+    (s.min_x..s.max_x).each do |i|
+      if target_y.nil? || -h > target_y || h < target_y
+        (-h).upto(h) do |j|
+          if target_y.nil? || s.y - j == target_y
+            print "."
+            self[i, s.y - j] = 0
+          end
+        end
+      end
+      h += s.x > i ? 1 : -1
+    end
+    puts
+  end
+
+  def count_empty(target_y)
+    sensors.each do |s|
+      fill_y(s, target_y)
+    end
+    grid.values.map { |v| v[target_y] }.count { |v| v == 0 }
+  end
+
+  def fill_y(s, y)
+    s.x_range_for_y(y, min_x, max_xy)&.each do |x|
+      self[x, y] = 0
+    end
+  end
+
+  def x_ranges_for_y(y)
+    ranges = sensors.map do |s|
+      s.x_range_for_y(y, min_x, max_y)
+    end
+    ranges.compact!
+    ranges.sort_by!(&:begin)
+  end
+
+  def coverage_for(y)
+    p y
+    x_ranges_for_y(y).reduce([]) do |ranges, range|
+      if ranges.empty?
+        ranges.push range
+        next ranges
+      end
+      last_range = ranges.pop
+      new_range = merge_range(last_range, range) || range
+      ranges.push new_range
+      ranges
+    end.first
+  end
+
+  def clamp_range(range)
+    [range.begin, min_x].max..[range.end, max_x].min
+  end
+
+  def merge_range(range, other)
+    if range.cover?(other.begin) && range.cover?(other.end)
+      range
+    elsif range.cover?(other.begin)
+      range.begin..other.end
+    elsif range.cover?(other.end)
+      other.begin..range.end
+    else
+      nil
+    end
+  end
+
+  def find_hole
+    (min_y..max_y).map do |y|
+      cov = coverage_for(y)
+      next if cov.begin == min_x
+      p (cov.begin - 1) * 4_000_000 + y
+      exit
+    end
+  end
+end
+
+class Sensor < Struct.new(:x, :y, :beacon)
+  def to_s
+    "S"
+  end
+
+  def dx = (beacon.x - x).abs
+  def dy = (beacon.y - y).abs
+  def min_x = x - dx - dy
+  def max_x = x + dx + dy
+  def min_y = y - dx - dy
+  def max_y = y + dx + dy
+
+  def x_range_for_y(target_y, from_x, to_x)
+    return if target_y > max_y || target_y < min_y
+    target_y_diff = (y - target_y).abs
+
+    [min_x + target_y_diff, from_x].max..[max_x - target_y_diff, to_x].min
+  end
+end
+
+class Beacon < Struct.new(:x, :y)
+  def to_s
+    "B"
+  end
+end
+
+scan = Scan.new(min_x: 0, max_x: 20, min_y: 0, max_y: 20)
+scan.read(input)
+# scan.fill
+# scan.draw
+scan.find_hole
+
+# scan = Scan.new(min_x: 0, max_x: 4_000_000, min_y: 644594, max_y: 4_000_000)
+# scan.read(File.read("15.txt"))
+# # p scan.count_empty(2000000)
+# scan.find_hole
